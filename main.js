d61a5d63e5d3560139d3188c069f6d7ed6c68209
Hector Pincheira <hpincheira1@hotmail.com>
2023-07-29 09:53:39 -0400

    preEntrega 2, calculo de varios candidatos para el credito

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-const tasaActualAnual = 0.3;
-const LimiteEndeudamientoMensual = 0.25;
+let candidates = [];
 
 class Credito {
-    constructor(nombre, sueldo, montoCredito, meses, tasaActualAnual, LimiteEndeudamientoMensual) {
+    constructor(nombre, sueldo, montoCredito, meses) {
         this.nombre = nombre;
         this.sueldo = sueldo;
         this.credito = montoCredito;
         this.meses = meses;
-        this.tasaActualAnual = tasaActualAnual;
-        this.LimiteEndeudamientoMensual = LimiteEndeudamientoMensual;
+        this.tasaActualAnual = 0.3;
+        this.LimiteEndeudamientoMensual = 0.25;
         this.tasaActualMensual = this.tasaActualAnual / 12;
         this.montoCreditoMensual = this.credito / this.meses;
         this.EndeudamientoProyectado = this.montoCreditoMensual / this.sueldo;
@@ -17,11 +16,11 @@ class Credito {
         this.cuotaMensual = (() => {
             const factor = Math.pow((1 + this.tasaActualMensual), this.meses);
             return this.credito * (this.tasaActualMensual * factor) / (factor - 1);
-          })();
+        })();
         this.CreditoAPagarTotal = this.cuotaMensual * this.meses;
         this.calificar();
     }
-  
+
     calificar() {
         if (this.EndeudamientoProyectado>=this.LimiteEndeudamientoMensual){
             this.resultado = this.resultado + " Sobrepasas tu límite de endeudamiento mensual: ("+ this.LimiteEndeudamientoMensual + ") y tu score de endeudamiento es: (" + parseFloat(this.EndeudamientoProyectado.toFixed(2)) + ")";
@@ -32,21 +31,63 @@ class Credito {
             this.resultado = "Aprobado listo para retirar: " + " El crédito solicitado es: " + this.credito + ", Tiempo del crédito: " + this.meses + " meses, Tasa otorgada: " + this.tasaActualAnual + ", Cuota mensual a pagar: " + this.cuotaMensual.toFixed(0);
             this.EstadoCredito = true;
         }
+    
     }
 }
 
-let nombre = prompt("Ingresa tu nombre:");
-let montoSueldo = parseInt(prompt("Ingresa tu remuneración mensual:"));
-let montoCredito = parseInt(prompt("Ingresa el monto de tu credito para evaluar:"));
-let tiempo = parseInt(prompt("Ingresa cuantos meses pretendes pagar el credito:"));
+function askForCandidates() {
+    const numCandidates = document.getElementById('numCandidates').value;
 
-const persona1= new Credito(nombre, montoSueldo, montoCredito, tiempo, tasaActualAnual, LimiteEndeudamientoMensual);
+    for (let i = 0; i < numCandidates; i++) {
+        let candidateForm = document.createElement('form');
+        candidateForm.innerHTML = `
+            <h3>Candidato ${i + 1}</h3>
+            <input id="name${i}" type="text" placeholder="Nombre"><br>
+            <input id="sueldo${i}" type="number" placeholder="Remuneración mensual"><br>
+            <input id="credito${i}" type="number" placeholder="Monto de crédito"><br>
+            <input id="meses${i}" type="number" placeholder="Meses para pagar">
+        `;
+        document.getElementById('candidates').appendChild(candidateForm);
+    }
 
-if (persona1.EstadoCredito){
-    console.log("FELICITACIONES, " + persona1.nombre + " , tu credito está " + persona1.resultado + " y el total del credito a pagar será de: " +  persona1.CreditoAPagarTotal.toFixed(0));
-} else
-{
-    console.log("Que pena lamento informarte que tu credito está  " + persona1.resultado);
+    let submitButton = document.createElement('button');
+    submitButton.innerHTML = 'Evaluar';
+    submitButton.onclick = evaluateCandidates;
+    document.getElementById('candidates').appendChild(submitButton);
 }
 
-console.log(persona1);
+function evaluateCandidates() {
+    const numCandidates = document.getElementById('numCandidates').value;
+
+    for (let i = 0; i < numCandidates; i++) {
+        let nombre = document.getElementById(`name${i}`).value;
+        let sueldo = parseInt(document.getElementById(`sueldo${i}`).value);
+        let credito = parseInt(document.getElementById(`credito${i}`).value);
+        let meses = parseInt(document.getElementById(`meses${i}`).value);
+
+        let candidate = new Credito(nombre, sueldo, credito, meses);
+        candidates.push(candidate);
+    }
+
+    displayResults();
+}
+
+function displayResults() {
+    let results = document.getElementById('results');
+    results.innerHTML = '';
+
+    for (let i = 0; i < candidates.length; i++) {
+        let resultado="";
+        let candidateResult = document.createElement('p');
+        if (candidates[i].EstadoCredito){
+            resultado = "FELICITACIONES, " + candidates[i].nombre + " , tu credito está " + candidates[i].resultado + " y el total del credito a pagar será de: " +  candidates[i].CreditoAPagarTotal.toFixed(0);
+        }else{
+            resultado = `Que pena, ${candidates[i].nombre}: lamento informarte que tu credito está  ` + candidates[i].resultado
+        }
+
+        candidateResult.innerHTML = `Resultado para ${candidates[i].nombre}: ${resultado}`;
+        results.appendChild(candidateResult);
+    }
+}
+
+
