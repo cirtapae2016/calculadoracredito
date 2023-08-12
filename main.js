68109e81a109ea2d0e5e2fe5c51112ca09235d50
Hector Pincheira <hpincheira1@hotmail.com>
2023-08-12 19:32:19 -0400

    preEntrega 3, uso de boostrap para dejar mas limpia la pantalla.

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let numCandidatesGlobal = 0;
 let candidates = [];
 
+window.addEventListener('beforeunload', function() {
+    localStorage.removeItem('candidates');
+});
+
 class Credito {
     constructor(nombre, sueldo, montoCredito, meses) {
         this.nombre = nombre;
@@ -43,14 +47,24 @@ function askForCandidates() {
     numCandidatesGlobal += Number(numCandidates);
 
     for (let i = candidates.length; i < candidates.length + Number(numCandidates); i++) {
-        let candidateForm = document.createElement('form');
+       /* let candidateForm = document.createElement('form');
         candidateForm.innerHTML = `
             <h3>Candidato ${i + 1}</h3>
             <input id="name${i}" type="text" placeholder="Nombre"><br>
             <input id="sueldo${i}" type="number" placeholder="Remuneración mensual"><br>
             <input id="credito${i}" type="number" placeholder="Monto de crédito"><br>
             <input id="meses${i}" type="number" placeholder="Meses para pagar">
-        `;
+        `;  */
+        let candidateForm = document.createElement('form');
+        candidateForm.innerHTML = `
+        <div class="form-group">
+            <h3>Candidato ${i + 1}</h3>
+            <input class="form-control" id="name${i}" type="text" placeholder="Nombre"><br>
+            <input class="form-control" id="sueldo${i}" type="number" placeholder="Remuneración mensual"><br>
+            <input class="form-control" id="credito${i}" type="number" placeholder="Monto de crédito"><br>
+            <input class="form-control" id="meses${i}" type="number" placeholder="Meses para pagar">
+        </div>`;
+
         document.getElementById('candidates').appendChild(candidateForm);
     }
 }
@@ -63,28 +77,45 @@ function evaluateCandidates() {
         let meses = parseInt(document.getElementById(`meses${i}`).value);
 
         let candidate = new Credito(nombre, sueldo, credito, meses);
-        candidates.push(candidate);
+        candidates.push(candidate); // Agrega los candidatos recién ingresados a la variable global
     }
+
+    document.getElementById('candidates').innerHTML = ''; // Limpia la sección de ingreso de candidatos
 
     displayResults();
     guardarDatos(); // Llama a la función para guardar los datos
 }
 
+
 function displayResults() {
     let results = document.getElementById('results');
     results.innerHTML = '';
-
-    for (let i = 0; i < candidates.length; i++) {
+    let start = candidates.length - numCandidatesGlobal;
+    for (let i = start; i < candidates.length; i++) {
         let resultado = "";
-        let candidateResult = document.createElement('p');
-        if (candidates[i].EstadoCredito) {
-            resultado = "FELICITACIONES, " + candidates[i].nombre + " , tu credito está " + candidates[i].resultado + " y el total del credito a pagar será de: " + candidates[i].CreditoAPagarTotal.toFixed(0);
+        let cardHeaderClass = "bg-success text-white"; // Clase para tarjetas aprobadas
+        let cardBodyClass = "text-success";
+
+        if (!candidates[i].EstadoCredito) {
+            resultado = `Que pena, ${candidates[i].nombre}: lamento informarte que tu crédito está  ` + candidates[i].resultado;
+            cardHeaderClass = "bg-danger text-white"; // Clase para tarjetas rechazadas
+            cardBodyClass = "text-danger";
         } else {
-            resultado = `Que pena, ${candidates[i].nombre}: lamento informarte que tu credito está  ` + candidates[i].resultado;
+            resultado = "FELICITACIONES, " + candidates[i].nombre + " , tu crédito está " + candidates[i].resultado + " y el total del crédito a pagar será de: " + candidates[i].CreditoAPagarTotal.toFixed(0);
         }
 
-        candidateResult.innerHTML = `Resultado para ${candidates[i].nombre}: ${resultado}`;
-        results.appendChild(candidateResult);
+        let card = `
+        <div class="card">
+            <div class="card-header ${cardHeaderClass}">
+                Resultado para ${candidates[i].nombre}
+            </div>
+            <div class="card-body ${cardBodyClass}">
+                <p class="card-text">${resultado}</p>
+            </div>
+        </div>
+        <br>`;
+
+        results.innerHTML += card;
     }
 }
 
@@ -96,9 +127,43 @@ function traeMayorCreditoE() {
 }
 
 function MuestraMayorCredito() {
+    if (candidates.length === 0) {
+        let content = `
+            <div class="card text-white bg-warning text-center">
+                <div class="card-header">
+                    Información
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">No Hay Datos</h5>
+                    <p class="card-text">No hay información para mostrar. Por favor, ingrese candidatos primero.</p>
+                </div>
+            </div>
+        `;
+
+        document.getElementById('mayorCredito').innerHTML = content;
+        return; // Salimos de la función ya que no hay candidatos para procesar
+    }
+
     const MayorCreditoE = candidates.reduce((max, candidate) => (candidate.credito > max.credito) ? candidate : max, { credito: 0 });
-    document.getElementById('mayorCredito').innerHTML = `El préstamo más alto fue solicitado por ${MayorCreditoE.nombre} y es de ${MayorCreditoE.credito}`;
+    let content = `
+        <div class="card text-white bg-info text-center">
+            <div class="card-header">
+                Préstamo Más Alto
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${MayorCreditoE.nombre}</h5>
+                <p class="card-text">Este préstamo es el más alto y es de ${MayorCreditoE.credito}</p>
+            </div>
+            <div class="card-footer text-muted">
+                Otorgado por nuestra compañía
+            </div>
+        </div>
+    `;
+
+    document.getElementById('mayorCredito').innerHTML = content;
 }
+
+
 
 
 function guardarDatos() {
@@ -107,8 +172,7 @@ function guardarDatos() {
 
 function cargarDatos() {
     // Limpiamos las secciones de candidatos y resultados
-    document.getElementById('candidates').innerHTML = '';
-    document.getElementById('results').innerHTML = '';
+    limpiarPantalla();
 
     // Recuperamos los datos del almacenamiento local
     const datosGuardados = JSON.parse(localStorage.getItem('candidates'));
@@ -120,4 +184,11 @@ function cargarDatos() {
 
 function refrescarDatos() {
     localStorage.removeItem('candidates');
+}
+
+function limpiarPantalla() {
+    document.getElementById('numCandidates').value = '';
+    document.getElementById('candidates').innerHTML = '';
+    document.getElementById('results').innerHTML = '';
+    document.getElementById('mayorCredito').innerHTML = '';
 }
